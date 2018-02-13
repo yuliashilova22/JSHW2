a9f7cdc4e873b6772ac9539028734045ae09fe78
Yuliia Shylova <yuliashilova22@gmail.com>
2018-02-13 11:54:55 +0200

    hard commit

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+//Task 1
+function factorial(n) {
+	if (n == 0 || n == 1) {
+		return 1;
+	} else {
+		var fact = 1;
+		while (n != 0) {
+			fact *= n;
+			n--;
+		}
+		return fact;
+	}
+}
+
+console.log(factorial(5));
+
+
+//Task 2
+var numbers = [-1, -2, -3, -9, -2, -1, -3];
+
+function getMinSub(arr) {
+	var isPositive = arr.every(number => number >= 0);
+	if (isPositive) return [];
+
+	var minSum = arr[0];
+	var leftBound = 0;
+	var rightBound = 0;
+	var sum = 0;
+	var minusPosition = -1;
+
+	for(var i = 0; i < arr.length; i++) {
+		sum += arr[i];
+
+		if (sum <= minSum) {
+			minSum = sum;
+			leftBound = minusPosition + 1;
+			rightBound = i;
+		}
+
+		if (sum > 0) {
+			sum = 0;
+			minusPosition = i;
+		}
+	}
+	var minSub = arr.slice(leftBound, ++rightBound);
+	return minSub;
+}
+
+console.log(getMinSub(numbers));
+
